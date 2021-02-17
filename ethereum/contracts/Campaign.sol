835338c29511e324af7ead487bd8433c6ef3843e
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2021-02-17 22:13:44 +0200

    Added campaign tests

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -2,78 +2,77 @@ pragma solidity ^0.8.0;
 
 contract CampaignFactory {
     address[] public deployedCampaigns;
-    
-    function createCampaign(uint minimum) public {
+
+    function createCampaign(uint256 minimum) public {
         address newCampaign = address(new Campaign(minimum, msg.sender));
         deployedCampaigns.push(newCampaign);
     }
-    
+
     function getDeployedCampaigns() public view returns (address[] memory) {
         return deployedCampaigns;
     }
 }
 
 contract Campaign {
-    
     struct Request {
         string description;
-        uint value;
+        uint256 value;
         address payable recipient;
         bool complete;
-        uint approvalCount;
+        uint256 approvalCount;
         mapping(address => bool) approvals;
-    }   
-    
+    }
+
     address payable public manager;
-    uint public minimumContribution;
+    uint256 public minimumContribution;
     mapping(address => bool) public approvers;
-    uint public approversCount;
-    
-    uint numCampaigns;
-    mapping(uint => Request) public requests;
-    
-    constructor(uint minimum, address creator) {
+    uint256 public approversCount;
+
+    uint256 numCampaigns;
+    mapping(uint256 => Request) public requests;
+
+    constructor(uint256 minimum, address creator) {
         manager = payable(creator);
         minimumContribution = minimum;
     }
-    
+
     function createRequest(
-    string memory description, 
-    uint value, 
-    address payable recipient) public restricted payable returns (uint campaignId) {
+        string memory description,
+        uint256 value,
+        address payable recipient
+    ) public payable restricted returns (uint256 campaignId) {
         campaignId = numCampaigns++;
         Request storage request = requests[campaignId];
         request.description = description;
         request.value = value;
         request.recipient = recipient;
     }
-    
-    function approveRequest(uint campaignId) public {
-        
+
+    function approveRequest(uint256 campaignId) public {
         Request storage requestToApprove = requests[campaignId];
         //Check if is donator
         require(approvers[msg.sender]);
         //Check if has already voted
         require(!requestToApprove.approvals[msg.sender]);
-        
+
         requestToApprove.approvals[msg.sender] = true;
         requestToApprove.approvalCount++;
     }
-    
-    function finalizeRequest(uint campaignId) public {
+
+    function finalizeRequest(uint256 campaignId) public {
         Request storage requestToFinalize = requests[campaignId];
         require(!requestToFinalize.complete);
-        require(requestToFinalize.approvalCount > (approversCount/2));
+        require(requestToFinalize.approvalCount > (approversCount / 2));
         requestToFinalize.complete = true;
         requestToFinalize.recipient.transfer(requestToFinalize.value);
     }
-    
+
     function contribute() public payable {
         require(msg.value > minimumContribution);
         approvers[msg.sender] = true;
         approversCount++;
     }
-    
+
     modifier restricted {
         require(payable(msg.sender) == manager);
         _;
---
+++ ethereum/test/campaign.test.js
@@ -0,0 +1,75 @@
+const assert = require('assert');
+const ganache = require('ganache-core');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const compiledFactory = require('../build/CampaignFactory.json');
+const compiledCampaign = require('../build/Campaign.json');
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+	accounts = await web3.eth.getAccounts();
+
+	factory = await new web3.eth.Contract(compiledFactory.abi)
+		.deploy({ data: compiledFactory.evm.bytecode.object })
+		.send({ from: accounts[0], gas: 2000000 });
+
+	await factory.methods.createCampaign('100').send({
+		from: accounts[0],
+		gas: 1000000,
+	});
+
+	[campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+
+	campaign = await new web3.eth.Contract(compiledCampaign.abi, campaignAddress);
+});
+
+describe('Campaign', () => {
+	it('deploys a factory and a campaign', () => {
+		assert.ok(factory.options.address);
+		assert.ok(campaign.options.address);
+	});
+	it('marks caller as the campaign manager', async () => {
+		const manager = await campaign.methods.manager().call();
+		assert.strictEqual(manager, accounts[0]);
+	});
+	it('allows people to contribute ether', async () => {
+		const balanceBeforeContribute = await web3.eth.getBalance(campaignAddress);
+		await campaign.methods.contribute().send({
+			from: accounts[1],
+			value: '200',
+		});
+		const balanceAfterContribute = await web3.eth.getBalance(campaignAddress);
+		assert.ok(balanceAfterContribute - balanceBeforeContribute > 198);
+	});
+	it('adds contributors and marks them as approvers', async () => {
+		await campaign.methods.contribute().send({
+			from: accounts[1],
+			value: '200',
+		});
+		assert.ok(await campaign.methods.approvers(accounts[1]).call());
+	});
+	it('should require minimum contribution of 100 wei and does not add to approvers', async () => {
+		try {
+			await campaign.methods.contribute().send({
+				from: accounts[1],
+				value: '55',
+			});
+			assert(false);
+		} catch (error) {
+			assert.ok(!(await campaign.methods.approvers(accounts[1]).call()));
+		}
+	});
+	it('allows manager to make a payment request', async () => {
+		await campaign.methods
+			.createRequest('Test description', 200, accounts[2])
+			.send({ from: accounts[0], gas: 2000000 });
+		const request = await campaign.methods.requests(0).call();
+		assert.strictEqual(request.description, 'Test description');
+		assert.strictEqual(request.value, '200');
+	});
+});
