be16bf07313e30c403963a31ef58e8643df2dc79
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2021-03-03 16:14:49 +0200

    Fixed rendering of more than 1 campaign

--- client/src/app/campaign/campaign-list/campaign-list.component.ts
+++ client/src/app/campaign/campaign-list/campaign-list.component.ts
@@ -1,30 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { map } from 'rxjs/operators';
+import { mergeAll, scan } from 'rxjs/operators';
 import { AppState } from 'src/app/reducers';
 import { loadCampaigns } from '../campaign.actions';
 import { selectCampaigns } from '../campaign.selectors';
-
-// const ELEMENT_DATA = [
-//   {
-//     address: '0x48965416fa7sd98f7198af23s14df5',
-//     approvers: 45,
-//     balance: 550,
-//     requests: 3,
-//   },
-//   {
-//     address: '0xasfdgawe525352agsdr2q35asgdsf5',
-//     approvers: 45,
-//     balance: 100,
-//     requests: 3,
-//   },
-//   {
-//     address: '0x4896541asdgaw46azfh4tersatret4',
-//     approvers: 45,
-//     balance: 0.55,
-//     requests: 3,
-//   },
-// ];
 
 @Component({
   selector: 'app-campaign-list',
@@ -39,9 +18,11 @@ export class CampaignListComponent implements OnInit {
     this.store.dispatch(loadCampaigns());
     this.dataSource = this.store.pipe(
       select(selectCampaigns),
-      map((campaign) => {
-        return [{ campaign: campaign[0] }];
-      })
+      mergeAll(),
+      scan((acc: any[], value) => {
+        acc.push({ campaign: value });
+        return acc;
+      }, [])
     );
   }
 }
